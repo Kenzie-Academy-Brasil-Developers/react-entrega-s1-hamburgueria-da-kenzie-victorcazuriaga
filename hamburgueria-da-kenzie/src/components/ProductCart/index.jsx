a218f56591d9ac62582b43924d6d08bdd99871f5
Victor Cazuriaga <vikktorrv@gmail.com>
2022-06-14 11:33:47 -0300

    feature: mobile css

--- hamburgueria-da-kenzie/src/components/ProductCart/index.jsx
+++ hamburgueria-da-kenzie/src/components/ProductCart/index.jsx
@@ -3,18 +3,16 @@ import { useState } from "react";
 export default function ProductCart({ id, name, price, category, handleRemoveCart, img }) {
 
 
-    return (<>
+    return (
+        < div className="productCart" >
+            <img src={img} alt={name} />
+            <h1>{name}</h1>
+            <p> {category}</p>
+            <span>R${price}</span>
+            <button onClick={() => handleRemoveCart(id)}> Remover </button>
 
-        {
-            < div className="productCart" >
-                <img src={img} alt={name} />
-                <h1>{name}</h1>
-                <p> {category}</p>
-                <span>R${price}</span>
-                <button onClick={() => handleRemoveCart(id)}> Remover </button>
+        </div >
 
-            </div >}
-    </>
 
     )
 
